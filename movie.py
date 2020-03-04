53bb1aea110124c1b40794ebd03ae9745753a5bb
zhukeyi123 <1119805512@qq.com>
2020-03-04 10:11:48 +0800

    更新

--- movie.py
+++ movie.py
@@ -1,6 +1,14 @@
-import requests,webbrowser,prettytable
+import requests,webbrowser,prettytable,chardet
+def init():
+    url='https://www.ttupp.com/tv.txt'
+    r=requests.get(url)
+    r.encoding='GB2312'
+    global api,ports
+    api=r.json()['远程api']
+    ports=r.json()['播放源'][0]
+
 def search(keyword,port): #获取搜索结果
-    url='http://api.ttupp.com/cgi/qingfeng?type=search&jiekou='+port+'&value='+keyword
+    url=api+'?type=search&jiekou='+port+'&value='+keyword
     r=requests.get(url).json()
     data=r['data']
     return data
@@ -11,11 +19,11 @@ def prtres(result): #打印搜索结果
         tb.add_row([i+1,result[i]['sort'],result[i]['time'],result[i]['title']])
     print('共找到搜索结果 '+str(len(result))+' 条：')
     print(tb)
-    i=int(input('请输入结果前的序号选择： '))
+    i=int(input('请输入序号选择： '))
     return result[i-1]
 
 def detail(choice,port):
-    url='http://api.ttupp.com/cgi/qingfeng?type=video&jiekou='+port+'&value='+choice['url']
+    url=api+'?type=video&jiekou='+port+'&value='+choice['url']
     r=requests.get(url).json()
     content=r['data']['content']
     print('已选：【'+choice['title']+' / 共'+str(len(content))+'集】')
@@ -23,14 +31,19 @@ def detail(choice,port):
     for i in range(len(content)):
         tb.add_row([i+1,content[i]['name']])
     print(tb)
-    while True:
-        i=int(input('请输入剧集对应的序号选集： '))
-        webbrowser.open(content[i-1]['url'])
-        input('请不要关闭窗口，播放完后按Enter继续！')
-
+    i=input('请输入要下载的集数： ')
+    with open('./'+choice['title']+'_DownloadUrl.txt','a') as txt:
+        for item in content[:int(i)]:
+            txt.write(item['url']+'\n')
+    print('下载地址获取成功！已输出到当前目录下的 '+choice['title']+'_DownloadUrl.txt')
 if __name__ == "__main__":
+    init()
+    print('获取到播放源：')
+    for name,num in ports.items():
+        print(num,name)
+    port=input('请选择播放源（1，2，3...）： ')
     keyword=input('请输入搜索关键词： ')
-    port=input('请选择需要调用的接口（1，2，3）： ')
     result=search(keyword,port)
     choice=prtres(result)
     detail(choice,port)
+
