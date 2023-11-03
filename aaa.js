e0e8714e5bff8a0e84f61aa7371675131f18ba5a
zky104 <38574433+zky104@users.noreply.github.com>
2023-11-03 11:44:18 +0800

    Update aaa.js

--- aaa.js
+++ aaa.js
@@ -1,6 +1,11 @@
+name: Documents
+desc: Documents
+
 http:
+  mitm:
+    - "license.pdfexpert.com"
   script:
-    - match: license.pdfexpert.com
+    - match: ^https:\/\/license\.pdfexpert\.com\/api\/2\.0\/documents\/subscription\/refresh
       name: license.pdfexpert.com
       type: response # request / response
       require-body: true
@@ -10,6 +15,6 @@ http:
       max-size: 1048576 # 1MB
  
 script-providers:
-  your-fancy-script:
+  Documents.js:
     url: https://raw.githubusercontent.com/Yu9191/Rewrite/main/Documents.js
     interval: 86400
